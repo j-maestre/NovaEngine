ead3cfb31450677739bcbcb8fd084b2505904f73
xema <maestrequi@esat-alumni.com>
2025-04-21 13:18:13 +0100

    add camera component

---
+++ include/components/camera_component.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Core/defines.h"
+#include "memory"
+
+
+class CameraComponent {
+
+public:
+	CameraComponent();
+	CameraComponent(const CameraComponent&);
+	CameraComponent(CameraComponent&&);
+	~CameraComponent();
+
+	
+
+
+private:
+
+	
+
+};
---
+++ src/Components/camera_component.cpp
@@ -0,0 +1,18 @@
+
+#include "components/camera_component.h"
+
+CameraComponent::CameraComponent(){
+
+}
+
+CameraComponent::CameraComponent(const CameraComponent&)
+{
+}
+
+CameraComponent::CameraComponent(CameraComponent&&)
+{
+}
+
+CameraComponent::~CameraComponent()
+{
+}
